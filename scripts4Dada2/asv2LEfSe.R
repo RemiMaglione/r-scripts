2294fb83c72a6945edd68851fe5681808e8de38c
Rémi Maglione <remi.maglione@gmail.com>
2019-05-13 16:53:11 -0400

    Update asv2LEfSe.R
    Fix names for meta-class input to fit LEfSe requirement

--- scripts4Dada2/asv2LEfSe.R
+++ scripts4Dada2/asv2LEfSe.R
@@ -1,8 +1,8 @@
-asv2LEfSe <- function(ps.data, metalvl1=NA, metalvl2=NA){
+asv2LEfSe2 <- function(ps.data, subject=NA, class=NA, subclass=NA){
   ################################################################
   # Convert ASV embeded in phyloseq object to LEfSe table format #
   #       Developped By Rémi Maglione For Kembel Lab             #
-  # Thanks to Alexandre Naud for his advice in code efficiency   #
+  # Thanks To Alexandre Naud For it's improve in code efficiency #
   ################################################################
   # Create Tax vector
   tax_lefse <- apply(ps.data@tax_table@.Data, 1, function(x) paste(c(x), collapse = "|"))
@@ -18,7 +18,7 @@ asv2LEfSe <- function(ps.data, metalvl1=NA, metalvl2=NA){
   tax_asv <- tax_asv[,-1]
   
   # Extract meta data
-  meta <- t(ps.data@sam_data[,c(metalvl1, metalvl2)])
+  meta <- t(ps.data@sam_data[,c(subject, class)])
   
   # Join meta and tax_asv
   final_table <- rbind(meta, tax_asv)
