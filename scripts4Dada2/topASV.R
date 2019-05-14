6c2a0c5d84b0306fac06051f5e7886473282c649
Rémi Maglione <remi.maglione@gmail.com>
2019-05-14 18:34:07 -0400

    Create topASV.R

---
+++ scripts4Dada2/topASV.R
@@ -0,0 +1,9 @@
+topASV <- function(comm, tax, meta, top=20){
+  ps <-phyloseq(otu_table(comm, taxa_are_rows=FALSE),
+                tax_table(tax),
+                sample_data(meta))
+  top.names <- names(sort(taxa_sums(ps), decreasing=TRUE))[1:top] 
+  ps.top <- prune_taxa(top.names, ps) 
+  ps.top <- merge_phyloseq(ps.top, sample_data(meta))
+  return(ps.top)
+}
