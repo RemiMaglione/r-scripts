dd83f5b4fddb2796395a6cdde9c38696995efab2
Rémi Maglione <remi.maglione@gmail.com>
2019-06-12 13:59:53 -0400

    Create fasta2FakeAsv.R
    1rst dirty commit

---
+++ scripts4Dada2/fasta2FakeAsv.R
@@ -0,0 +1,42 @@
+fasta=NA
+j=1
+for (i in 1:length(rownames(Myfasta))) {
+  ifelse(substr(Myfasta[i+1,1], 1, 1)==">", 
+         NA,
+         {fasta[j]<-Myfasta[i,1]
+         fasta[j+1]<-Myfasta[i+1,1]
+         j=j+2
+         })
+}
+
+fasta<-data.frame(fasta)
+fasta$fasta <- as.character(fasta$fasta)
+
+j=1
+k=1
+sample.less50nts=NA
+seq.less50nts=NA
+
+sample=NA
+seq=NA
+
+for (i in 1:length(rownames(fasta))) {
+
+  ifelse(substr(fasta[i,1], 1, 1)==">", 
+         yes = ifelse(nchar(fasta[i+1,1])<50,
+                      yes = {
+                        sample.less50nts[k] <- fasta[i,1]
+                        seq.less50nts[k] <- fasta[i+1,1]
+                        k=k+1},
+                      no = {
+                        sample[j]<-fasta[i,1]
+                        seq[j]<-fasta[i+1,1]
+                        j=j+1}),
+         no = NA)
+}
+
+fasta.key <- data.frame(sample=sample, seq=seq)
+fasta.less50nts.key <- data.frame(sample=sample.less50nts, seq=seq.less50nts)
+
+seq.tab.fake <- t(data.frame(matrix(0, nrow = length(seq[!duplicated(seq)]), ncol = 1), row.names = seq[!duplicated(seq)]))
+rownames(seq.tab.fake) <- "fake"
