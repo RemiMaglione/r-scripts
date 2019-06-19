e918be3fb50ed4d13b9c3661fe70a5a9ca5f6944
Rémi Maglione <remi.maglione@gmail.com>
2019-06-19 16:28:55 -0400

    Create taxoMatch.R
    1rst commit

---
+++ scripts4taxonomy/taxoMatch.R
@@ -0,0 +1,15 @@
+taxoMatch <-  function(mock.table=mock.table, lda.table=lda.table, colname.match = "match"){
+  for (i in 1:length(rownames(mock.table))) {
+    ifelse(test = mock.table[i, "Genus"]%in%lda.table$Genus,
+           yes = ifelse(
+             test = !is.na(unlist(strsplit(mock.table$species[i],"/"))),
+             yes = ifelse(
+               test = unlist(strsplit(mock.table$species[i],"/"))%in%unlist(strsplit(lda.table$species[which(lda.table$Genus==mock.table$Genus[i])],"_")),
+               yes = mock.table[i, colname.match] <- "Oui",
+               no = mock.table[i, colname.match] <- "Non"),
+             
+             no = mock.table[i, colname.match] <- "Non"),
+           
+           no = mock.table[i, colname.match] <- "Non")
+    }
+}
