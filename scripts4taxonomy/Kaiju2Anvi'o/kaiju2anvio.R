1724b9e2cfcca4207b8c291217de254f0d630ac3
Rémi Maglione <remi.maglione@gmail.com>
2020-08-06 16:37:57 -0400

    Create kaiju2anvio.R
    1rst commit

---
+++ scripts4taxonomy/Kaiju2Anvi'o/kaiju2anvio.R
@@ -0,0 +1,43 @@
+#!/usr/bin/env Rscript
+args = commandArgs(trailingOnly=TRUE)
+parallel=TRUE
+
+if (length(args)==0) {
+  stop("At least one argument must be supplied (input kaiju file).n", call.=FALSE)
+} else if (length(args)==1) {
+  # default output file
+  args[2] = "kaiju2Anvio-fixed.names"
+} else if (length(args)==3) {
+  # default output file
+  parallel = args[3]
+}
+
+kaiju2mat <- function(kaiju.names, parallel) {
+  require(parallel)
+  ifelse(test = isTRUE(parallel),
+         yes = {
+           cores <- detectCores()
+           cores <- cores-1
+         },
+         no = cores <- parallel)
+  
+  mat <- matrix(unlist(mclapply(1:nrow(kaiju.names), FUN = function(i) {
+    ifelse(!kaiju.names[i, 4]=="",
+           yes = {x.tmp <- unlist(strsplit(x = as.character(kaiju.names[i, 4]), split = ";"))
+           length(x.tmp) <- 7
+           return(x.tmp)
+           },
+           no = {x.tmp <- NA
+           length(x.tmp) <- 7
+           return(x.tmp)
+           })
+  }, mc.cores = cores)), ncol = 7, byrow = TRUE)
+  mat <- cbind(as.matrix(kaiju.names[,2]), mat)
+  colnames(mat) <- c("seqname", "superkingdom","phylum","class","order","family","genus","species")
+  return(mat)
+}
+
+kaiju.names <- read.table(file = args[1], sep = "\t", fill = TRUE, row.names = NULL, header = FALSE, quote = "")
+kaijumat<-kaiju2mat(kaiju.names=kaiju.names, parallel = parallel)
+write.table(kaijumat, file = args[2], quote = FALSE, col.names = FALSE, row.names = FALSE)
+
