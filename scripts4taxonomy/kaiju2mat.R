2d5b863dc8ea9012b4f2316b47adeddf59a223e3
Rémi Maglione <remi.maglione@gmail.com>
2020-08-11 10:18:54 -0400

    Update kaiju2mat.R
    Update the main function

--- scripts4taxonomy/kaiju2mat.R
+++ scripts4taxonomy/kaiju2mat.R
@@ -5,22 +5,35 @@
 # Working r input: table.taxo <- read.table(file = "kaiju.names.out", sep = "\t", fill = TRUE, row.names = NULL, header = FALSE, quote = "") 
 ###########Codded by Rémi Maglione for Kembel Lab###########
 
-require(parallel)
-cores <- detectCores()
+#Parallel package install control
+if (!require("parallel")) install.packages("parallel")
 
-kaiju2mat <- function(table.taxo) {
+#Function
+kaiju2mat <- function(table.taxo, parallel) {
+  require(parallel)
+  ifelse(test = isTRUE(parallel),
+         yes = {
+           cores <- detectCores()
+           cores <- cores-1
+         },
+         no = cores <- parallel)
+  
   mat <- matrix(unlist(mclapply(1:nrow(table.taxo), FUN = function(i) {
     ifelse(!table.taxo[i, 4]=="",
            yes = {x.tmp <- unlist(strsplit(x = as.character(table.taxo[i, 4]), split = ";"))
            length(x.tmp) <- 7
            return(x.tmp)
            },
-           no = {x.tmp <- table.taxo[i, 4]
+           no = {x.tmp <- NA
            length(x.tmp) <- 7
            return(x.tmp)
            })
-    }, mc.cores = cores-1)), ncol = 7, byrow = TRUE)
+  }, mc.cores = cores)), ncol = 7, byrow = TRUE)
   mat <- cbind(as.matrix(table.taxo[,2]), mat)
-  colnames(mat) <- c("seqname", "superkingdom","phylum","class","order","family","genus","species")
+  colnames(mat) <- c("gene_callers_id","t_domain","t_phylum","t_class","t_order","t_family","t_genus","t_species")
   return(mat)
-  }
+}
+
+#Main USAGE
+#table.taxo <- read.table(file = "kaiju.names.out", sep = "\t", fill = TRUE, row.names = NULL, header = FALSE, quote = "") 
+#table.taxo.mat <- kaiju2mat(table.taxo)
