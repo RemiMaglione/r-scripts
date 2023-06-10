44fe129b927329f24f48c528f1bf9bcc7f32309c
Rémi Maglione <remi.maglione@gmail.com>
2023-06-10 09:47:33 -0400

    Create seqTable2Fasta.R
    1rst commit

---
+++ scripts4genomics/seqTable2Fasta.R
@@ -0,0 +1,12 @@
+require(Biostrings)
+
+convertTableToFasta <- function(sequence_table, output_file) {
+  # Create a DNAStringSet object from the sequence_table
+  dna_sequences <- DNAStringSet(sequence_table$oligo)
+  
+  # Set the names of the DNAStringSet using the sequence names
+  names(dna_sequences) <- sequence_table$name
+  
+  # Write the DNAStringSet to a FASTA file
+  writeXStringSet(dna_sequences, file = output_file, format = "fasta")
+}
