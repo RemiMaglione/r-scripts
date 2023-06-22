7c3e077c45df553d905c159e9e70d13e19ab6b00
Rémi Maglione <remi.maglione@gmail.com>
2023-06-22 15:23:00 -0400

    Update MyGenbank2Taxo.R
    Retry are now managed with a while loop

--- scripts4taxonomy/MyGenbank2Taxo.R
+++ scripts4taxonomy/MyGenbank2Taxo.R
@@ -1,20 +1,19 @@
 MyGenbank2Taxo <- function(genbankIDList, max_retries=5){
   require('taxize')
   # Initialize variables
-  retry_count <- 0
   max_retries <- max_retries
   failed_queries <- vector("character", length(genbankIDList))
-  result <- vector("list", length(genbankIDList))
+  result <- list()  # Initialize an empty list
   print("Querring NCBI Started")
   for (i in seq_along(genbankIDList)) {
-    # Check if the query failed previously
-    if (i > retry_count) {
-      # Retry the query
+    
+    retry_count <- 0 #initiate retry count
+    
+    while (retry_count <= max_retries) {
       query_result <- tryCatch(
         classification(genbank2uid(id = genbankIDList[i]), db = "ncbi"),
         error = function(e) {
           message(paste("Query", genbankIDList[i], "failed. Retrying..."))
-          failed_queries[i] <<- TRUE  # Mark the query as failed
           return(NULL)  # Return NULL for failed queries
         }
       )
@@ -24,21 +23,19 @@ MyGenbank2Taxo <- function(genbankIDList, max_retries=5){
         # Process the successful result
         result[[genbankIDList[i]]] <- query_result
         
-        # Reset the retry count
-        retry_count <- 0
+        # Reset the failed query flag
+        failed_queries[i] <- FALSE
+        
+        break  # Exit the retry loop if successful
       } else {
         # Increment the retry count
         retry_count <- retry_count + 1
-        
-        # Check if the maximum number of retries has been reached
-        if (retry_count >= max_retries) {
-          message(paste("Maximum number of retries reached for query", genbankIDList[i]))
-          break  # Exit the loop if maximum retries reached
-        }
-        
-        # Skip to the next iteration
-        next
       }
+    }
+    
+    # Check if the maximum number of retries has been reached without success
+    if (retry_count > max_retries) {
+      message(paste("Maximum number of retries reached for query", genbankIDList[i]))
     }
     # If the loop reaches this point, it means the iteration was skipped due to a failed query
   }
